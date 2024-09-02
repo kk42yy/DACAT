4e85cbda230a6309fe4a6ef453e64e592f5fbe95
kk42yy <161680482+kk42yy@users.noreply.github.com>
2024-09-02 23:19:16 +0800

    Update train.sh

--- src/M2CAI16/train.sh
+++ src/M2CAI16/train.sh
@@ -11,4 +11,4 @@ python3 train.py phase --split cuhk2714 --backbone convnextv2 --freeze --workers
 ## Step 2
 ## You need to use trained model in Step 1, and saved in .../train_scripts/newly_opt_ykx/LongShortNet/long_net_convnextv2.pth.tar
 # python3 train_longshort.py phase --split cuhk2714 --backbone convnextv2 --workers 4 --seq_len 64 --lr 1e-5 --random_seed --trial_name DACAT \
-#     --epochs 100
+#     --epochs 30
